d9d0faffabe11f1a7c7d0f187497457226075fdf
Juamez <juanmendezs.a@gmail.com>
2023-07-06 12:37:10 -0500

    Add auth and logout logic

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,18 +5,11 @@ const bcrypt = require('bcrypt')
 const crypto = require('crypto')
 const mongoose = require('mongoose')
 const { createUserCredentials } = require('../models/models')
-const router = express.Router()
 
 // const runClient = require('../mongoConnection')
-const {uri, db} = require('../constants/index')
+const db = mongoose.connection
+const {uri} = require('../constants/index')
 require('dotenv').config()
-
-
-
-db.on('error', console.error.bind(console, 'Conection error: '))
-db.once('open', () => {
-	console.log('connected to mongodb')
-})
 
 async function runClient() {
 	try {
@@ -32,11 +25,15 @@ async function runClient() {
 
 runClient()
 
+db.on('error', console.error.bind(console, 'Conection error: '))
+db.once('open', () => {
+	console.log('connected to mongodb in auth')
+})
 
 passport.use(
-  new LocalStrategy(async (username, password, done) => {
+  new LocalStrategy(async function verify (username, password, done) {
     try {
-        const user = await db.collection("user_credentials").findOne({ name: username })
+        const user = await db.collection("user_credentials").findOne({ username: username })
         if(!user) return done(null,false, { message: 'Incorrect username.' })
 
         const isPasswordValid = await bcrypt.compare(password, user.password)
@@ -51,46 +48,60 @@ passport.use(
 
 passport.serializeUser((user, done) => {
   process.nextTick(() => {
-    done(null, {id: user.id, username: user.username})
+    return done(null, {id: user._id, username: user.username})
   })
 })
 
-passport.deserializeUser(async(user, done) => {
+passport.deserializeUser((user, done) => {
   process.nextTick(() => {
-    return cb(null, user);
+    return done(null, user);
   });
 })
 
+const router = express.Router()
 
-// router.get('/login', (req, res, next) => {
-//   res.json({name: 'login'})
-// })
+router.get('/login', (req, res, next) => {
+  res.json({name: 'login', "session": req.session})
+})
 
-// router.post('/login/password', passport.authenticate('local', {successRedirect: "/", failureRedirect: '/login'}), (req, res) => {
-//   res.json({message: 'loggin succesfully', user: req.user})
-// })
+router.post('/login/password', passport.authenticate('local'), (req, res, next) => {
+    const user = {
+      id: req.user._id
+    }
 
-// router.post('/logout', (req, res, next) => {
-//   req.logout((err) => {
-//     if (err) { return next(err); }
-//     res.redirect('/');
-//   });
-// });
+    if(req.user.name) { user.name = req.user.name}
+    if(req.user.username) { user.username = req.user.username}
+    res.json({ "user": user, "cookies": req.cookies })
+  }
+)
+
+router.get('/logout', (req, res, next) => {
+  res.json({"session": req.session})
+})
+
+router.post('/logout', passport.authenticate('session'), (req, res, next) => {
+  req.logout(async (err) => {
+    if (err) { return next(err)}
+    req.session.destroy()
+    const deleteSession = await db.collection("user_store_session").deleteOne({"session.passport.user.username": "alice"}) 
+    return deleteSession
+  })
+  res.end()
+});
 
 router.get('/signup', (req, res, next) => {
   res.json({message: 'signup'})
 })
 
-router.post('/signup', async (req, res, next) => {
+router.post('/signup', passport.initialize(), async (req, res, next) => {
   try {
     const {username, password} = req.body
-    console.log(username, password)
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
     const user = await createUserCredentials(username, hashedPassword)
     req.login(user, (err) => {
       if(err) return next(err)
-      res.json({user: user})
+      res.json({user})
     })
   } catch(error) {
     res.status(500).json({err: 'An error ocurred', error})
@@ -99,11 +110,16 @@ router.post('/signup', async (req, res, next) => {
   }
 })
 
-router.get('/logout', (req, res, next)=> {
-  req.logout((err) => {
-    if(err) return next(err)
-    res.redirect('/')
-  })
+router.get('/session', passport.authenticate('session'), async (req, res, next) => {
+  try {
+    const session = await db.collection("user_store_session").findOne({"session.passport.user.username": "alice"})
+    if(!session) return next(null, false, {message: "not session was found"})
+
+    res.json({ "session": session })
+    return next(null, session)
+  } catch (err) {
+    console.error("method get from session gave an error", err)
+  }
 })
 
 module.exports = router

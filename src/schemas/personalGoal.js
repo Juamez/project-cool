568d30dceacb40c39c696c345dc029b80b593c04
Juamez <juanmendezs.a@gmail.com>
2023-07-11 03:02:50 -0500

    Update type for reminder and goal type

--- src/schemas/personalGoal.js
+++ src/schemas/personalGoal.js
@@ -3,12 +3,13 @@ const { Schema } = mongoose
 
 const personalGoalSchema = new Schema({
   _id: Schema.Types.ObjectId,
+  owner_id: { type: Schema.Types.ObjectId, ref: "UserStoreSession"},
   title: String,
   description: String,
-  goal_type: {type: Schema.Types.ObjectId, ref:"GoalType"},
+  goal_type: {type: String, ref:"GoalType"},
   start_date_goal: {type: Date, default: Date.now },
   end_date_goal: Date,
-  reminder_type: {type: Schema.Types.ObjectId, ref:"Reminder"},
+  reminder_type: {type: String, ref:"Reminder"},
 })
 
 const PersonalGoal = mongoose.model('PersonalGoal', personalGoalSchema)
